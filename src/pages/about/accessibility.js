d3006e1e3f9004042943acceb23a1d2b7e97c1c4
Jeff Coburn <jeff.coburn@gmail.com>
2019-12-17 16:43:40 -0500

    Update accessibility.js

--- src/pages/about/accessibility.js
+++ src/pages/about/accessibility.js
@@ -24,7 +24,7 @@ const AccessibilityPage = ({
    
        <div className="w-100 pa2">
        <h1>Accessibility Policy</h1>
-        
+        <h2>jeff edited this</h2>
 <p>We are committed to making this site accessible to all our users. We adhere to Section 508 of the Rehabilitation Act and the W3C Web Content Accessibility Guidelines.</p>
 
 <p>Accessibility includes many facets and best practices. The overall goal is to ensure that our website can be used and easily understood by a diverse array of people, with a range of abilities and disabilities, using a wide range of devices, operating systems, and assistive technology platforms. We have designed this site to be useful, usable, and easy to navigate for all users.</p>
