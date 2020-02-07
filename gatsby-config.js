2f9ccd20a9a40e8d1c25f9179fdde7fc3b6e01da
jrepo <julie.reposa@gmail.com>
2020-02-06 21:25:44 -0500

    more debugging

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,19 +83,26 @@ plugins: [
         },
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
+        `gatsby-plugin-catch-links`,
   `gatsby-plugin-styled-components`,
   {
     resolve: `gatsby-plugin-manifest`,
     options: {
       name: `gatsby-starter-default`,
       short_name: `starter`,
-      //start_url: `/`,
+      start_url: `/`,
       background_color: `#663399`,
       theme_color: `#663399`,
       display: `minimal-ui`,
       icon: `src/images/ici-icon.png`, // This path is relative to the root of the site.
     },
   },
+  {
+    resolve: 'gatsby-remark-copy-linked-files',
+    options: {
+    destinationDir: 'public',
+    },
+    },
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
   // `gatsby-plugin-offline`,
