16520beac0766e1e22044defaa1377f5cbfa8740
Jeff Coburn <jeff.coburn@gmail.com>
2021-07-22 14:15:46 -0400

    Update NavLinks.js

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -91,6 +91,7 @@ const activeLinkStyle = {
              <li><Link to="/projects/catada">CATADA</Link></li>
              <li><Link to="/projects/nercve">NERCVE</Link></li>
              <li><Link to="/projects/emp1stfl">Employment First Florida</Link></li>
+             <li><Link to="/projects/gator">GATOR</Link></li>
         </ul>
       </div>
 
