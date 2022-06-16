2154c629bb17d879f68bffed36d1912643071faf
Jeff Coburn <jeff.coburn@umb.edu>
2022-06-16 15:41:32 +0100

    Update jobsListTemplate.js

--- src/templates/jobsListTemplate.js
+++ src/templates/jobsListTemplate.js
@@ -38,7 +38,7 @@ const crumbLabelArr = customCrumbLabel.split('/');
             />
           </div>
         <h1 className="page-title">Job Openings at ICI</h1>
-       <p> {data.currentBuildDate.currentDate}</p>
+       <p> Current Build: {data.currentBuildDate.currentDate}</p>
       <div className="post-list">
         {posts.map(post => (
           <div key={post.node.id} className="post-list__item border-bottom border-1 border-dark">
@@ -88,13 +88,14 @@ export const pageQuery = graphql`query GetJobsPosts($limit: Int, $skip: Int) {
     currentDate 
   }
   allMarkdownRemark(
+    
     limit: $limit
     sort: {fields: [frontmatter___date], order: ASC}
     skip: $skip
     filter: {
         frontmatter: {
           posttype: {eq: "jobs"}
-          close_date: { lte: currentBuildDate   }
+          close_date: { lte: $currentDate   }
         }
       }
   ) {
