2f3bbb9f3a48be89e2883377a2d31a6811c6d5f8
jrepo <julie.reposa@gmail.com>
2023-12-15 14:09:14 -0500

    airtable updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -34,8 +34,8 @@ Email: <a href="mailto:ici@umb.edu">ici@umb.edu</a><br />
 <div className="col-md-4 col-sm-12 p-2 ">
   <h3 className="fs-4 border-bottom border-light pv-0 lh-lg text-uppercase">Links</h3>
   <ul className="align-middle w-100 w-75-l list pl0 list-unstyled">
-    <li> <Link to="/about/" className="link  whited-inline-block" title="About">About ICI</Link></li>
-     <li><Link to="/about/areas-of-emphasis" className="link white d-inline-block" title="Areas of Emphasis">Areas of Emphasis</Link></li>
+<li> <Link to="/about/" className="link  whited-inline-block" title="About">About ICI</Link></li>
+<li><Link to="/about/areas-of-emphasis" className="link white d-inline-block" title="Areas of Emphasis">Areas of Emphasis</Link></li>
     <li> <Link to="/projects" className="link white d-inline-block" title="Projects">Projects</Link></li>
        <li>   <Link to="/services" className="link white d-inline-block" title="Services">Services</Link></li>
        <li> <a href="https://lp.constantcontactpages.com/su/jlCEExQ/subscribe" className="link white d-inline-block" target = "_blank" rel = "noopener noreferrer" title="Subscribe">Subscribe</a></li>
