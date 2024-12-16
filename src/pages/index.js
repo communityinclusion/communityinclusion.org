d5d5c6d2e1c33d60d24ef532d33963a191d57c13
jrepo <julie.reposa@gmail.com>
2024-12-16 10:11:28 -0500

    reconfig homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import education_logo from '../images/education-logo.png'
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+ import Select from '@mui/material/Select';
 // import { kebabCase } from 'lodash';
 
 const IndexPage = ({data, location }) => {
@@ -27,89 +27,79 @@ const IndexPage = ({data, location }) => {
     <HomeLayout location={location} title={siteTitle}>
        
 
-
-
-    <h2 className="bb bw1 border-bottom border-dark border-2 pb-1">Areas of Emphasis</h2>
+     <section className="center">
+    <h2 className="border-bottom border-2 border-dark pb-3">Areas of Emphasis</h2>
     
-    
-    <div className="row">
-  <article className="col-lg-6 col-md-12  d-flex align-self-stretch   mb-4">
-  <div className="card">
-    <div className="card-header bg-green my-0 px-3 ">
+ <div className="row my-4 ">
+ <div className="col-md-3 d-flex align-self-stretch bg-white p-1">
+<div className="card mb-2">
+    <div className="card-header bg-green">
     <img src={employment_logo} className="d-inline-block align-middle" alt = "Employment icon" />
-    <h3 className="fs-3 text-white  d-inline-block align-middle ps-2">     Employment</h3>
+    <h3 className="fs-4 text-white d-inline-block align-middle ps-2"> Employment</h3>
     </div>
-    <div className="card-body">
+    <div className="card-body d-flex flex-column px-0 pb-0">
       <p className="lh-copy">
     Building a rewarding career in the community should be an expectation for all citizens. With support and planning, people with significant disabilities can thrive in the workplace.
       </p>
-      <p><Link className="float-end" to="/areas-of-emphasis/employment"> Learn More About Employment &gt;&gt;</Link></p>
+     <p className="mt-auto"><Link to="/areas-of-emphasis/employment"> Learn More About Employment &gt;&gt;</Link></p>
     </div>
     </div>
-  </article>
+ </div>
 
 
-
-  <article className="col-lg-6 col-md-12  d-flex align-self-stretch  mb-4">
-  <div className="card">
-  <div className="card-header bg-purple mv-0 p-2 ">
+ <div className="col-md-3 d-flex align-self-stretch bg-white p-1">
+  <div className="card mb-2">
+  <div className="card-header bg-purple">
   <img src={education_logo} className="d-inline-block align-middle" alt = "Education icon" /> 
-  <h3 className="text-white d-inline-block align-middle ps-2"> Education</h3>
+  <h3 className="fs-4 text-white d-inline-block align-middle ps-2"> Education</h3>
     </div>
-    <div className="card-body">
+    <div className="card-body d-flex flex-column px-0 pb-0">
       <p>
    From pre-K through university, schools are engaging and involving students with a range of disabilities. By building skills early, young people can exit high school prepared to pursue higher education and find jobs that match their interests.
       </p>
-      <p><Link className="float-end" to="/areas-of-emphasis/education"> Learn More About Education &gt;&gt;</Link></p>
+      <p className="mt-auto"><Link to="/areas-of-emphasis/education"> Learn More About Education &gt;&gt;</Link></p>
     </div>
     </div>
-  </article>
-  </div> 
-  
-   <div className="row">
+ </div>
 
-
-  <article className="col-lg-6 col-md-12  d-flex align-self-stretch  mb-4 ">
-    <div className="card">
-  <div className="card-header bg-red mv-0 px-3 ">
+ <div className="col-md-3 d-flex align-self-stretch bg-white p-1">
+ <div className="card mb-2">
+  <div className="card-header bg-red">
   <img src={community_logo} className="d-inline-block align-middle" alt = "Community icon" /> 
-    <h3 className="fs-3 text-white d-inline-block align-middle ps-2"> Community Life</h3>
+    <h3 className="fs-4 text-white d-inline-block align-middle ps-2"> Community Life</h3>
    </div>
-    <div className="card-body">
+    <div className="card-body d-flex flex-column px-0 pb-0">
       <p>
    Outside of work and school, people with disabilities are involved in a wide range of activities that make up a satisfying life. Whether it’s volunteering at a music festival, getting involved with a faith community, or attending a sports event, there’s much more to life than a job.
       </p>
-      <p><Link className="float-end" to="/areas-of-emphasis/community"> Learn More About Community Life &gt;&gt;</Link></p>
+      <p className="mt-auto"><Link to="/areas-of-emphasis/community"> Learn More About Community Life &gt;&gt;</Link></p>
     </div>
     </div>
-  </article>
+</div>
 
 
-
-  <article className="col-lg-6 col-md-12 d-flex align-self-stretch  mb-4">
-  <div className="card">
-  <div className="card-header bg-orange mv-0 px-3 ">
-  <img src={healthcare_logo} className="d-inline-block align-middle" alt = "Healthcare icon" /> 
-    <h3 className="text-white d-inline-block align-middle ps-2"> Health Care</h3>
+ <div className="col-md-3 d-flex align-self-stretch bg-white p-1">
+  <div className="card mb-2">
+  <div className="card-header bg-orange">
+ <img src={healthcare_logo} className="d-inline-block align-middle" alt = "Healthcare icon" />
+    <h3 className="fs-4 text-white d-inline-block align-middle ps-2">  Health Care</h3>
    </div>
-    <div className="card-body">
+    <div className="card-body d-flex flex-column px-0 pb-0">
     <p>Accessing quality health care enables us to live productively and to receive support when facing mental, behavioral, or physical challenges. For people with disabilities, equitable health care can require additional advocacy and planning.
       </p>
-      <p><Link className="float-end" to="/areas-of-emphasis/healthcare"> Learn More About Health Care &gt;&gt;</Link></p>
+      <p className="mt-auto"><Link to="/areas-of-emphasis/healthcare"> Learn More About Health Care &gt;&gt;</Link></p>
     </div>
     </div>
-  </article>
-
-
-
   </div>
+ </div>
+ </section>
 
 
 
-  <div className="row my-4 ">
-  <div className="col-lg-9 col-md-8 col-sm-12 bg-white p-3">
+<div className="row my-4 ">
+  <div className="col-md-8 bg-white p-3">
   <section className="center">
-          <h2 className="border-bottom border-2 border-dark">
+          <h2 className="border-bottom border-2 border-dark pb-1">
             New at ICI
           </h2>
         
@@ -144,8 +134,32 @@ const IndexPage = ({data, location }) => {
           <div className="fr pt2"><Link to="/news">View More</Link></div>
         </section>
         </div>
-   
-        <div className="col-lg-3 col-md-4 col-sm-12 tc">
+
+  <div className="col-md-4 bg-white p-3">
+  <section className="center">
+          <h3 className="border-bottom border-2 border-dark pb-1">
+            Featured Project
+          </h3>
+          <div className="card shadow-sm p-1">
+				<div className="card-body">
+				<h4 className="card-title"><a href="/projects/michet">
+				Massachusetts Center for Inclusive Higher Education and Transition  </a>
+				</h4>
+				<p>The Massachusetts Center for Inclusive Higher Education and Transition promotes the advancement of higher education and transition services for people with intellectual and developmental disabilities (IDD) and autism.</p>
+				</div>
+	 </div>
+  </section>
+  
+
+
+ 
+  <section className="center mt-4">
+          <h3 className="border-bottom border-2 border-dark">
+           Staff 
+          </h3>
+        
+
+  <div className="tc">
         <div className="staffDropdown">
         <FormControl variant="filled" className="formControl ">
           <InputLabel id="demo-simple-select-label tc">Staff Directory</InputLabel>
@@ -167,8 +181,13 @@ const IndexPage = ({data, location }) => {
   </FormControl>
               
           </div>
-     </div>
-     </div>
+     </div>  
+</section>  
+</div>
+    </div>
+ 
+
+  
   </HomeLayout>
   );}
 }
@@ -185,7 +204,7 @@ export const pageQuery = graphql`{
   allMarkdownRemark(
     sort: {frontmatter: {date: DESC}}
     filter: {frontmatter: {posttype: {eq: "news"}}}
-    limit: 3
+    limit: 5
   ) {
     edges {
       node {
