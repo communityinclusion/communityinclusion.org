54b10645c71fa67fc2cd032af3afd22e3359abe6
Jeff Coburn <jeff.coburn@umb.edu>
2025-02-14 18:47:25 -0500

    new front page stories

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import employment_logo from '../images/employment-logo.png'
 import education_logo from '../images/education-logo.png'
 import emp_infograph from "../images/graphic1_ICI_emp.png"
 import ed_infograph from "../images/graphic2_ICI_ed.png"
+import ek_photo from "../images/stories/CO-Demonstration-2021_crop.jpg"
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
@@ -100,7 +101,24 @@ Research has shown that people with disabilities are an untapped, qualified reso
  </div>
  </section>
 
+ <section className="center pb-3">
+ <div className="bg-light card">
+ <h2 className="border-bottom border-2 border-dark pb-3">Independence In Action
+ <br /><span className="h3">Speaking with Confidence and Humor</span> </h2>
+ <div className="row">
+    <div className="col-sm-9">
+    <p>After undergoing a total laryngectomy that left her with no voice, E.K., a 71-year-old ICU nurse, was referred for an Augmentative Alternative Communication (AAC) evaluation by the Division of Vocational Rehabilitation.</p>
 
+<p>Ms. K. found that the Wego 7A by TalkToMe Technologies supported her communication in several different ways. Through training, Ms. K. learned to use the pre-set word buttons to build novel responses in a conversation, ask and answer questions, and support her social interactions.</p>
+
+<p><strong>Searching for employment has been Ms. K.'s priority.</strong> While waiting for the perfect nursing position, Ms. K. has successfully worked with local food delivery services, communicating regularly with customers, restaurant staff, and her employers using her AAC device. When asked how her AAC device has impacted her daily life, smiling, she uses her AAC device to reply, “I can order food in a drive-thru, and be funny too”. Regarding returning to her career, Ms. K. emphatically demands, “I can do everything I used to be able to do, except talk.”</p>
+    </div>
+    <div className="col-sm-3"><img className="img-fluid border border-2 border-primary" src={ek_photo} alt="Ms E. K. with her  Augmentative Alternative Communication (AAC) device" />
+    <p className="mt-auto"><Link to="/publications/stories/"><strong> More stories about Independence in Action &gt;&gt; </strong></Link></p></div>
+  </div>
+
+ </div>
+  </section>
 
 
 
@@ -148,7 +166,7 @@ Research has shown that people with disabilities are an untapped, qualified reso
    </div>
     <div className="card-body d-flex flex-column px-0 pb-0">
       <p>
-   Outside of work and school, people with disabilities are involved in a wide range of activities that make up a satisfying life. Whether it’s getting involved with a faith community, attending a sports event, or volunteering at a local charity, people with disabilities participate in their community.
+   Outside of work and school, people with disabilities are involved in a wide range of activities that make up a satisfying life. Whether it's getting involved with a faith community, attending a sports event, or volunteering at a local charity, people with disabilities participate in their community.
       </p>
       <p className="mt-auto"><Link to="/areas-of-emphasis/community"> Learn More About Community Life &gt;&gt;</Link></p>
     </div>
---
+++ src/pages/publications/stories/index.md
@@ -0,0 +1,16 @@
+---
+title: "Independence in Action"
+date: 2025-02-14
+path: "/publications/stories/"
+posttype: "page"
+---
+
+<section class="center pb-3">
+    <div class="bg-light card">
+        <img class="img-fluid" src="/images/stories/src/images/stories/CO-Demonstration-2021.png" alt="Ms E. K. with her  Augmentative Alternative Communication (AAC) device">
+        <h2>Speaking with Confidence and Humor</h2>
+        <p>After undergoing a total laryngectomy that left her with no voice, E.K., a 71-year-old ICU nurse, was referred for an Augmentative Alternative Communication (AAC) evaluation by the Division of Vocational Rehabilitation.</p>
+        <p>Ms. K. found that the Wego 7A by TalkToMe Technologies supported her communication in several different ways. Through training, Ms. K. learned to use the pre-set word buttons to build novel responses in a conversation, ask and answer questions, and support her social interactions.</p>
+        <p><strong>Searching for employment has been Ms. K.'s priority.</strong> While waiting for the perfect nursing position, Ms. K. has successfully worked with local food delivery services, communicating regularly with customers, restaurant staff, and her employers using her AAC device. When asked how her AAC device has impacted her daily life, smiling, she uses her AAC device to reply, “I can order food in a drive-thru, and be funny too”. Regarding returning to her career, Ms. K. emphatically demands, “I can do everything I used to be able to do, except talk.”</p>
+     </div>
+</section>
