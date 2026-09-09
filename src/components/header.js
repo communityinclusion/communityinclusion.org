4d85d69c259f0cf4d333254255761cd79bcfc311
jrepo <julie.reposa@gmail.com>
2026-09-09 15:47:35 -0400

    changing ICI header

--- src/components/header.js
+++ src/components/header.js
@@ -2,129 +2,91 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import GoogleCustomSearchBox from "./GoogleCustomSearchBox.jsx"
-import logo from "../images/ici-150w.png"
-
-// import Search from './search.js'
-// import SearchForm from "./searchForm"
-// <SearchForm className="col-lg-12" /> //
+import iciLogo from "../images/ici-150w.png"
+import employmentTile from "../images/ici-employment.png"
+import educationTile from "../images/ici-education.png"
+import communityTile from "../images/ici-community.png"
+import healthcareTile from "../images/ici-healthcare.png"
 
 const Header = () => (
   <header>
-    <div className="container-xxl header">
-      <div className="row">
-        <div className="col-lg-9 col-sm-12 col-12 mt-2 d-flex site-logo-col">
+    <div className="ici-top-header d-none d-lg-block">
+      <div className="container-xxl d-flex justify-content-between align-items-center">
+        <ul className="list-inline ps-0 m-0 header-links d-flex">
+          <li className="list-inline-item">
+            <a
+              href="https://lp.constantcontactpages.com/sl/VAsWN3m/ici"
+              className="link dim dark-gray f6"
+              title="Subscribe"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Subscribe
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <Link
+              to="/about/contact/"
+              className="link dim dark-gray f6"
+              title="Contact"
+            >
+              Contact
+            </Link>
+          </li>
+          <li className="list-inline-item">
+            <Link
+              to="/donate/"
+              className="link dim dark-gray f6"
+              title="Donate"
+            >
+              Donate
+            </Link>
+          </li>
+        </ul>
+
+        <div className="ici-top-header-search">
+          <GoogleCustomSearchBox />
+        </div>
+      </div>
+    </div>
+
+    <div className="container-xxl">
+      <div className="ici-banner-wrap text-center">
+        <h1 id="banner-text" className="title">
+          ICI at the University of Massachusetts Boston
+        </h1>
+        <nav className="banner" aria-label="ICI focus areas">
           <Link
-            to="/"
-            className = "d-flex align-items-center w-100 mid-gray text-decoration-none link site-logo-link"
-            title="Home"
+            className="banner__tile"
+            to="/about/areas-of-emphasis/employment/"
           >
-            <img className="site-logo-img" alt="ICI" src={logo} />
-            <span className = "blue fw-bold flex-grow-1 text-center site-logo-text" >
-             ICI at the University of
-              <br />
-              Massachusetts Boston
-            </span>
+            <img src={employmentTile} alt="Employment" />
           </Link>
-        </div>
-
-        <div className="col-lg-3  d-none d-lg-block  p-2">
-          <div className="row">
-            <ul className="col-lg-12 list-inline ps-0 m-0 mb-2 header-links d-flex justify-content-around">
-              <li className="flex-item list-inline-item pr1">
-                <a
-                  href="https://lp.constantcontactpages.com/sl/VAsWN3m/ici"
-                  className="link dim dark-gray f6  "
-                  title="Subscribe"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Subscribe{" "}
-                </a>{" "}
-              </li>{" "}
-              <li className="flex-item list-inline-item   pr1 pl1">
-                {" "}
-                <Link
-                  to="/about/contact/"
-                  className="link dim dark-gray f6  "
-                  title="Contact"
-                >
-                  Contact{" "}
-                </Link>{" "}
-              </li>
-              <li className="flex-item list-inline-item  ps-1">
-                {" "}
-                <Link
-                  to="/donate/"
-                  className="link dim dark-gray f6  "
-                  title="Donate"
-                >
-                  Donate{" "}
-                </Link>{" "}
-              </li>
-            </ul>
-
-           {/* <ul className="col-lg-12 social-links d-flex justify-content-around list-inline pl0 m-0 mb-2">
-              <li className="list-inline-item flex-item ">
-                {" "}
-                <a
-                  href="https://twitter.com/ICInclusion"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Twitter"
-                >
-                  <FontAwesomeIcon icon={faTwitter} size="2x" />
-                </a>{" "}
-              </li>
-              <li className="list-inline-item flex-item ">
-                {" "}
-                <a
-                  href="https://www.instagram.com/communityinclusion/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Instagram"
-                >
-                  <FontAwesomeIcon icon={faInstagram} size="2x" />
-                </a>{" "}
-              </li>{" "}
-              <li className="list-inline-item flex-item  pl1">
-                {" "}
-                <a
-                  href="https://communityinclusion.tumblr.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Tumblr"
-                >
-                  <FontAwesomeIcon icon={faTumblrSquare} size="2x" />
-                </a>{" "}
-              </li>{" "}
-              <li className="list-inline-item flex-item  pl1">
-                {" "}
-                <a
-                  href="https://www.facebook.com/communityinclusion/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="Facebook"
-                >
-                  <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
-                </a>{" "}
-              </li>{" "}
-              <li className="list-inline-item flex-item  pl1">
-                {" "}
-                <a
-                  href="https://www.youtube.com/user/communityinclusion"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="YouTube"
-                >
-                  <FontAwesomeIcon icon={faYoutube} size="2x" />
-                </a>{" "}
-              </li>{" "}
-            </ul> */}
-            <div>
-              <GoogleCustomSearchBox />
-            </div>
-          </div>
-        </div>
+          <Link
+            className="banner__tile"
+            to="/about/areas-of-emphasis/education/"
+          >
+            <img src={educationTile} alt="Education" />
+          </Link>
+          <Link className="banner__tile banner__tile--ici" to="/" title="Home">
+            <img src={iciLogo} alt="Institute for Community Inclusion" />
+          </Link>
+          <Link
+            className="banner__tile"
+            to="/about/areas-of-emphasis/community/"
+          >
+            <img src={communityTile} alt="Community Life" />
+          </Link>
+          <Link
+            className="banner__tile"
+            to="/about/areas-of-emphasis/healthcare/"
+          >
+            <img src={healthcareTile} alt="Health Care" />
+          </Link>
+        </nav>
+        <h2 id="banner-tagline" className="tagline h5">
+          <em>advancing access and opportunity for people with disabilities</em>
+        </h2>
       </div>
     </div>
   </header>
